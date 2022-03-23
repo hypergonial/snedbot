cc8743bc08908f875b76ea17a8c935f7f9734e4c
HyperGH <46067571+HyperGH@users.noreply.github.com>
2022-03-23 23:44:38 +0100

    Fix migration script

--- db/migrations/2.sql
+++ db/migrations/2.sql
@@ -3,3 +3,5 @@ UPDATE button_roles SET buttonstyle = 'PRIMARY' WHERE buttonstyle = 'Blurple';
 UPDATE button_roles SET buttonstyle = 'SECONDARY' WHERE buttonstyle = 'Grey';
 UPDATE button_roles SET buttonstyle = 'SUCCESS' WHERE buttonstyle = 'Green';
 UPDATE button_roles SET buttonstyle = 'DANGER' WHERE buttonstyle = 'Red';
+
+UPDATE schema_info SET schema_version = 2;
