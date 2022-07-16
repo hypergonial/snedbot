eb22eeb94530b676e4c9c884cbbd7cdb6e9d3ce5
HyperGH <46067571+HyperGH@users.noreply.github.com>
2022-07-16 16:09:31 +0200

    Use app permissions in check

--- models/checks.py
+++ models/checks.py
@@ -5,6 +5,7 @@ import hikari
 import lightbulb
 
 from models.context import SnedContext
+from models.context import SnedSlashContext
 from models.errors import BotRoleHierarchyError
 from models.errors import RoleHierarchyError
 from utils import helpers
@@ -111,23 +112,33 @@ async def _has_permissions(ctx: SnedContext, *, perms: hikari.Permissions) -> bo
 
 async def _bot_has_permissions(ctx: SnedContext, *, perms: hikari.Permissions) -> bool:
     _guild_only(ctx)
-    try:
-        channel, guild = (ctx.get_channel() or await ctx.app.rest.fetch_channel(ctx.channel_id)), ctx.get_guild()
-    except hikari.ForbiddenError:
-        raise lightbulb.BotMissingRequiredPermission(
-            "Check cannot run due to missing permissions.", perms=hikari.Permissions.VIEW_CHANNEL
-        )
-    if guild is None:
-        raise lightbulb.InsufficientCache("Some objects required for this check could not be resolved from the cache.")
-    member = guild.get_my_member()
-    if member is None:
-        raise lightbulb.InsufficientCache("Some objects required for this check could not be resolved from the cache.")
 
-    if isinstance(channel, hikari.GuildThreadChannel):
-        channel = ctx.app.cache.get_guild_channel(channel.parent_id)
+    if isinstance(ctx, lightbulb.SlashContext):
+        bot_perms = ctx.interaction.app_permissions
+        assert bot_perms is not None
+    else:
+        try:
+            channel, guild = (ctx.get_channel() or await ctx.app.rest.fetch_channel(ctx.channel_id)), ctx.get_guild()
+        except hikari.ForbiddenError:
+            raise lightbulb.BotMissingRequiredPermission(
+                "Check cannot run due to missing permissions.", perms=hikari.Permissions.VIEW_CHANNEL
+            )
+        if guild is None:
+            raise lightbulb.InsufficientCache(
+                "Some objects required for this check could not be resolved from the cache."
+            )
+        member = guild.get_my_member()
+        if member is None:
+            raise lightbulb.InsufficientCache(
+                "Some objects required for this check could not be resolved from the cache."
+            )
 
-    assert isinstance(channel, hikari.GuildChannel)
-    missing_perms = ~lightbulb.utils.permissions_in(channel, member) & perms
+        if isinstance(channel, hikari.GuildThreadChannel):
+            channel = ctx.app.cache.get_guild_channel(channel.parent_id)
+        assert isinstance(channel, hikari.GuildChannel)
+        bot_perms = lightbulb.utils.permissions_in(channel, member)
+
+    missing_perms = ~bot_perms & perms
     if missing_perms is not hikari.Permissions.NONE:
         raise lightbulb.BotMissingRequiredPermission(
             "The bot is missing one or more permissions required in order to run this command", perms=missing_perms
