c5b1005d3a68e293ecbab645120e19fd36707324
HyperGH <46067571+HyperGH@users.noreply.github.com>
2022-09-05 19:17:11 +0200

    Add tester notice send

--- extensions/fallingfrontier.py
+++ extensions/fallingfrontier.py
@@ -1,21 +1,91 @@
 import hikari
 import lightbulb
+import miru
 
 from config import Config
+from etc import constants as const
 from models.bot import SnedBot
+from models.context import SnedSlashContext
 from models.plugin import SnedPlugin
 
+TESTER_STAGING_ROLE = 939665137328095242 #971843694896513074
+TESTER_STAGING_CHANNEL = 956313445802852412 #971844463884382259
+FF_GUILD = 861603964642525204 #684324252786360476
+
+TEST_NOTICE = """
+**You are being contacted in regards to the Falling Frontier Tester Recruitment.**
+
+First of all, congratulations.
+As mentioned during the initial application process, we are expanding the test team which is currently focused on core gameplay mechanics and you have been selected to join the current testing group.
+
+Before you can begin testing, you will have to sign an **NDA**, the process of which will be handled by Todd of Stutter Fox Studios and Tim from Hooded Horse.
+
+If you would like to take part and agree to the requirement of signing an NDA, you will be able to coordinate with both Todd and the senior testing team in the `#tester-lounge` once you have received the appropriate roles.
+
+Once the NDA has been signed you will be be given additional permissions, allowing you to go through the setup process for **GitHub** and receive your key through **Steam**.
+
+Should you have any questions regarding the process or testing, feel free to ask in the `#tester-staging` channel after accepting this invitation.
+
+Thank you!
+
+*Notice: This is an automated message, replies will not be read.*
+"""
+
 ff = SnedPlugin("Falling Frontier")
-ff.default_enabled_guilds = Config().DEBUG_GUILDS or (684324252786360476, 813803567445049414)
+ff.default_enabled_guilds = Config().DEBUG_GUILDS or (FF_GUILD, 813803567445049414)
 
 
 @ff.listener(hikari.GuildMessageCreateEvent)
 async def hydrate_autoresponse(event: hikari.GuildMessageCreateEvent) -> None:
-    if event.guild_id not in (684324252786360476, 813803567445049414):
+    if event.guild_id not in (FF_GUILD, 813803567445049414):
         return
 
     if event.content and event.content == "Everyone this is your daily reminder to stay hydrated!":
         await event.message.respond("<:FoxHydrate:851099802527072297>")
+
+
+@ff.listener(miru.ComponentInteractionCreateEvent)
+async def handle_test_invite(event: miru.ComponentInteractionCreateEvent) -> None:
+    if not event.custom_id.startswith("FFTEST:") or event.guild_id is not None:
+        return
+
+    if event.custom_id == "FFTEST:ACCEPT":
+        await event.context.defer()
+        await event.app.rest.add_role_to_member(FF_GUILD, event.context.user, TESTER_STAGING_ROLE)
+        await event.context.respond(embed=hikari.Embed(title="Tester Invite Accepted", description=f"Please see <#{TESTER_STAGING_CHANNEL}> for further instructions.\n\nThank you for participating!", color=const.EMBED_GREEN))
+        await event.app.rest.create_message(TESTER_STAGING_CHANNEL, f"{event.user.mention} accepted the testing invitation! Welcome! <:FoxWave:851099801608388628>", user_mentions=True)
+
+    elif event.custom_id == "FFTEST:DECLINE":
+        await event.context.respond(embed=hikari.Embed(title="Tester Invite Declined", description="Thank you for your interest in the Falling Frontier Testing Program.", color=const.ERROR_COLOR))
+        await event.app.rest.create_message(TESTER_STAGING_CHANNEL, f"`{event.user}` declined the testing invitation.")
+    
+    view = miru.View.from_message(event.context.message)
+    for item in view.children:
+        assert isinstance(item, miru.Button)
+        item.disabled = True
+
+    await event.context.edit_response(components=view)
+
+
+@ff.command
+@lightbulb.app_command_permissions(hikari.Permissions.ADMINISTRATOR, dm_enabled=False)
+@lightbulb.option("recipients", "A list of all users to send the notice to, one user per line. Format: User#1234", type=hikari.Attachment)
+@lightbulb.command("sendtestnotice", "Send out tester notice to new people.", guilds=Config().DEBUG_GUILDS or (FF_GUILD,), pass_options=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def send_test_notice(ctx: SnedSlashContext, recipients: hikari.Attachment) -> None:
+    await ctx.respond(hikari.ResponseType.DEFERRED_MESSAGE_CREATE)
+
+    converter = lightbulb.converters.UserConverter(ctx)
+    users = [await converter.convert(user_str) for user_str in (await recipients.read()).decode("utf-8").splitlines()]
+    view = miru.View().add_item(miru.Button(label="Accept", style=hikari.ButtonStyle.SUCCESS, emoji="✔️", custom_id="FFTEST:ACCEPT")).add_item(miru.Button(label="Decline", style=hikari.ButtonStyle.DANGER, emoji="✖️", custom_id="FFTEST:DECLINE"))
+    failed = []
+    for user in users:
+        try:
+            await user.send(TEST_NOTICE, components=view)
+        except (hikari.ForbiddenError, hikari.NotFoundError):
+            failed.append(user)
+
+    await ctx.respond(f"Sent testing notice to **{len(users) - len(failed)}/{len(users)}** users.\n\n**Failed to send to:** ```{'\n'.join(failed) if failed else 'All users were sent the notice.'}```")
 
 
 def load(bot: SnedBot) -> None:
